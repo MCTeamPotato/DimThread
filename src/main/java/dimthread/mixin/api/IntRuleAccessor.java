06274b2969d4d2c84bb4948de55b4c71a1a3a138
Kasualix <670067575@qq.com>
2023-06-10 19:28:01 +0800

    import clean

--- src/main/java/dimthread/mixin/api/IntRuleAccessor.java
+++ src/main/java/dimthread/mixin/api/IntRuleAccessor.java
@@ -1,9 +1,8 @@
 package dimthread.mixin.api;
 
+import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-
-import net.minecraft.world.GameRules;
 
 @Mixin(GameRules.IntRule.class)
 public interface IntRuleAccessor {
