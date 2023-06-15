d7b8f9052b0cdcb31db38c9cbd9b624f7ef9825e
Kasualix <670067575@qq.com>
2023-06-15 15:22:21 +0800

    Update MinecraftServerMixin.java

--- src/main/java/dimthread/mixin/MinecraftServerMixin.java
+++ src/main/java/dimthread/mixin/MinecraftServerMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.GameRules;
+import net.minecraftforge.fml.hooks.BasicEventHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -63,11 +64,13 @@ public abstract class MinecraftServerMixin {
 			}
 
 			DimThread.swapThreadsAndRun(() -> {
+				BasicEventHooks.onPreWorldTick(serverWorld);
 				try {
 					serverWorld.tick(shouldKeepTicking);
 				} catch(Throwable throwable) {
 					crash.set(new CrashInfo(serverWorld, throwable));
 				}
+				BasicEventHooks.onPostWorldTick(serverWorld);
 			}, serverWorld, serverWorld.getChunkManager());
 		});
 
